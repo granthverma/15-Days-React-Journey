4ba48f17c41da8e607dd36767e3d0007805bf133
Your Name <himanshuvermanewtonsch@gmail.com>
2023-11-27 18:54:02 +0530

    counter app

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+// set value of counter  useState koi bhi value defaluts value
+//two value  f - count
+// setcount s - its fnf is variable updated karne ke liya 
+  // let counter = 5
+
+
+  const addValue = () => {
+    if (counter <100) {
+      counter =  counter+1
+      setCounter(counter)
+
+      
+    } else {
+      alert("You've reached the maximum value (100)!");
+      
+    }
+
+  
+  } ;
+
+
+  const removeValue = () => {
+
+    if (counter > 0) {
+      counter =  counter-1
+    setCounter(counter)
+      
+    }  else {
+      alert("You've reached the minimum value (0)!");
+      
+    }
+    
+
+    
+  } ;
+
+  
+  const ResetValue = () => {
+    
+    counter = 0 ;
+    setCounter(counter)
+    
+  };
+
+  return (
+    <>
+      
+      <h1>The-Counter</h1>
+      <h2>Counter Value :{counter} </h2>
+      <h3> The Counter count the value  0 to 100</h3>
+
+
+      <button onClick={addValue}>Increment </button>
+      
+     <button onClick={removeValue}>decrement</button>
+      
+        
+<br/>
+      <button onClick={ResetValue}> Reset Value</button> 
+    </>
+  )
+}
+
+
+// hooks is updated hoga data  , import { useState } from 'react' yhi se aata hooks 
+
+export default App
