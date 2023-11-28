a74656fe3d7d8493384d38978e8be57e913e0b16
Your Name <himanshuvermanewtonsch@gmail.com>
2023-11-28 16:49:08 +0530

    css compoments

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,28 +8,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      {/* <h1 className='BlinkMacSystemFont p-4 bg-red-400  text-black rounded-xl'>Tailwind</h1>
+      
+      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img className="w-24 h-24 rounded-full mx-auto" src="https://granthcodes.com/uploads/about_04.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 t space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
       </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Himanshu Verma
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+     frontend dev  , Granthcodes.com
+      </div>
+    </figcaption>
+  </div>
+</figure> */}
+
     </>
   )
 }
+
+// class - className use , jsx img should be closed 
 
 export default App
