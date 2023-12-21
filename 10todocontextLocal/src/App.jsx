668704f69c0f929609c3629721584053b09d6e27
Your Name <himanshuvermanewtonsch@gmail.com>
2023-12-21 21:23:25 +0530

    add functionality todo app

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,18 +1,52 @@
 import { useState } from 'react'
+import {TodoProvider} from "./context"
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const[todos , setTodos]=useState([])
+ 
+const addTodo = (todo)=>{
+
+  setTodos((prev) => [{id:Date.now() , ...todo}] , ...prev)
+
+}
+const updatedTodo = (id ,todo)=>{
+  setTodos((prev)=> prev.map((prevTodo)=>(prev.Todo.id === id ?todo :prev)) )
+  
+}
+
+
+const deleteTodo = (todo)=>{
+  setTodos((prev)=> prev.filter ((todo)=> todo.id !==id))
+}
+
+
+
+const toggleComplete = (todo)=>{
+  setTodos((prev) => prev.map ((prevTodo) =>
+  prevTodo.id ===id ?{...prevTodo,
+    completed:!prevTodo.completed
+  
+  }:prevTodo))
+  
+}
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
+    <TodoProvider>
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
        
-    </>
+    </TodoProvider>
   )
 }
 
