524c4549aea4f961b10d584313266b953178a0ac
Your Name <himanshuvermanewtonsch@gmail.com>
2023-12-22 16:33:54 +0530

    add local storage

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {TodoProvider} from "./context"
+import './App.css'
 
 import './App.css'
 
@@ -25,19 +26,25 @@ function App() {
  }:prevTodo)))
 }
 
+useEffect (()=>{
 
+  const todos =JSON.parse(localStorage.getItem("todos"))
 
+  if (todos && todos.length >0) {
+    setTodos(todos)
+  }
+},[])
+ 
+    
 
-
-
-
-
-           
+useEffect(()=>{
+  localStorage.setItem("todos", JSON.stringify(todos))
+},[todos])
   
 
 
   return (
-    <TodoProvider>
+    <TodoProvider value ={{todos ,addTodo ,updateTodo ,deleteTodo ,toggleComplete}}>
      <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
