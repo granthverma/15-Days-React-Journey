81d90e95ee7ae23a1d532e156c3e9ab7eacaff60
Your Name <himanshuvermanewtonsch@gmail.com>
2023-12-24 02:23:01 +0530

    todofrom

--- 10todocontextLocal/components/TodoFrom.jsx
+++ 10todocontextLocal/components/TodoFrom.jsx
@@ -1,35 +1,38 @@
-import React, { useState } from 'react'
-import {useTodo} from '../context/TodoContext'
+// Import necessary dependencies from React and TodoContext
+import React, { useState } from 'react';
+import { useTodo } from '../context/TodoContext';
 
-
+// Define the functional component TodoForm
 function TodoForm() {
- const [todo , setTodo]=useState(""),
+    // State to manage the input value for the new todo
+    const [todo, setTodo] = useState("");
 
- 
+    // Destructure the addTodo function from the TodoContext
+    const { addTodo } = useTodo();
 
- const {addTodo}=useTodo()
+    // Event handler for adding a new todo
+    const add = (e) => {
+        e.preventDefault();
 
+        // If todo is empty, return early
+        if (!todo) return;
 
- const add =(e)=>{
-    e.preventDefault()
+        // Call addTodo function to add a new todo with default completion status as false
+        addTodo({ todo, completed: false });
 
-    if(!todo) return 
-    addTodo({todo,completed : false})
-    setTodo("")
- }
-   
+        // Reset the todo input value to an empty string
+        setTodo("");
+    };
 
-    
-
-   
+    // JSX structure for the TodoForm component
     return (
-        <form  onSumbit ={add}className="flex">
+        <form onSubmit={add} className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
                 value={todo}
-                onChange={(e)=> setTodo(e.target.value)}
+                onChange={(e) => setTodo(e.target.value)}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
@@ -38,4 +41,5 @@ function TodoForm() {
     );
 }
 
+// Export the TodoForm component as the default export
 export default TodoForm;
