ad4916ddd1d175f29ba3a8bd44566f3f50ad4136
Your Name <himanshuvermanewtonsch@gmail.com>
2024-01-02 18:06:50 +0530

    todos

--- reduxToolkitTodo/src/components/Todos.jsx
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -3,16 +3,19 @@ import{ useSelector , useDispatch} from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
-    const todos =useSelector (state =>state.todos)
-    const dispatch =useDispatch()
+    const todos = useSelector (state =>state.todos)
+    const dispatch = useDispatch()
 
     return (
         <>
         <div>Todos</div>
-          <li key={todo.id}>
-            {todo.text}
-     <button onClick={()=>dispatch(removeTodo(todo.id))}></button>
-          </li>
+        {todoSlice.map((todo)=>(
+           <li key={todo.id}>
+           {todo.text}
+    <button onClick={() => dispatch(removeTodo(todo.id))}></button>
+         </li>
+        ))}
+         
           </>
         
     )
