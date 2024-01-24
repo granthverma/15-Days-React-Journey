9a1002be349a30d12a60c8db4e1f3d765c6891ab
Your Name <himanshuvermanewtonsch@gmail.com>
2024-01-24 23:58:49 +0530

    add comment

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,21 +1,33 @@
+// Importing the `useState` hook from React
 import { useState } from 'react'
 
+// Importing styles from the 'App.css' file
 import './App.css'
+
+// Importing the 'AddTodo' and 'Todos' components from their respective files
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
 
+// The main functional component 'App'
 function App() {
-  
+  // No state is being used in this component, but you can add state using the `useState` hook if needed
 
+  // Return the JSX representing the component
   return (
     <>
-  <h1 className="text-4xl font-bold text-center text-blue-700 mt-12 mb-6">Learn about redux toolkit</h1>
+      {/* Page Title */}
+      <h1 className="text-4xl font-bold text-center text-blue-700 mt-12 mb-6">
+        Learn about redux toolkit
+      </h1>
 
-
+      {/* Adding Todo form component */}
       <AddTodo />
-      <Todos />  
+
+      {/* Displaying Todos component */}
+      <Todos />
     </>
   )
 }
 
+// Exporting the 'App' component as the default export
 export default App
